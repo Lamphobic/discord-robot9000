f08d11ef22542b6dfacd2726779cfd42f90d6a22
lamphobic <kongregamer@gmail.com>
2019-08-25 20:39:23 -0700

    i'm stupid

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.HJDBuilder;
 
 public class Main {
+    public static void main(String[] args){
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        String token = "PihoCLytOG9oS3henjkNQVY0cVNwL9iy";
+        builder.setToken(token);
+    }
 }
